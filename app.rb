7f6e04e0de63c5dd7e3381d57e1a5f79bcd65957
Andrew <andrew.k.jordan@gmail.com>
2013-05-28 13:45:29 -0700

    Tidys up the message and code style

--- app.rb
+++ app.rb
@@ -13,55 +13,53 @@ RestClient.enable Rack::Cache,
   :entitystore => Dalli::Client.new
 
 # base URL for the app
-get_or_post '/' do
+get_or_post "/" do
 
 # an array of San Francisco spot IDs
 spot_ids = ["113", "649", "648", "114", "117"]
 
-if params["Body"].include? "spots" 
+incoming_sms = params["Body"].chomp.downcase
+
+if incoming_sms.include?("spots")
 
 	# query Splitcast API and store JSON of spots
-	response = RestClient.get 'http://api.spitcast.com/api/county/spots/san-francisco/', {:accept => :json}
+	response = RestClient.get("http://api.spitcast.com/api/county/spots/san-francisco/", :accept => :json)
 	all_spots = JSON.parse(response)
 
 	# create string called spots to store values from JSON
-	spots = String.new
+	spots = ""
 
 	# iterate over all_spots and pull out spot_name and spot_id
-	all_spots.each { |i| spots << "#{i["spot_name"]}: #{i["spot_id"]} " }
+	all_spots.each { |i| spots << "#{i["spot_name"]}: #{i["spot_id"]} \n" }
 
 	# build Twilio response
 	response = Twilio::TwiML::Response.new { |r| r.Sms "#{spots}" }
 
-elsif spot_ids.downcase.include? params["Body"]
+elsif spot_ids.include?(incoming_sms)
 
 	# query Splitcast API and store JSON of conditions for a given spot
-	response = RestClient.get "http://api.spitcast.com/api/spot/forecast/#{params["Body"]}/", {:accept => :json}
+	response = RestClient.get("http://api.spitcast.com/api/spot/forecast/#{incoming_sms}/", :accept => :json)
 	all_conditions = JSON.parse(response)
 
 	# create string called conditions to store values from JSON
-	conditions = String.new
+	conditions = ""
 
 	# iterate over all_conditions and pull out hour, shape_full, and size
 	# parse "gmt" from JSON, parse it into a DateTime and compare it to the system time represented in GMT to get only the nearest 4 predictions
 	all_conditions.each do |i|
 	  time_difference = (DateTime.parse(i["gmt"]).to_time.to_i - (Time.now.getgm.to_i))
 	  if (time_difference >= 0 && time_difference <= 14400)
-	    conditions << "#{i["hour"]}: #{i["size"]}ft shape: #{i["shape_full"]} / "
+	    conditions << "#{i["hour"]}: #{i["size"]}ft, #{i["shape_full"]}\n"
 	  end
 	end
 
 	# build Twilio response
-	response = Twilio::TwiML::Response.new do |r|
-	  r.Sms "The conditions at #{all_conditions.first["spot_name"]} will be: #{conditions}"
-	end	
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Conditions for #{all_conditions.first["spot_name"]}\n* Time: size, shape *\n#{conditions}" }
 
 else
-	
+
 	# build Twilio response
-	response = Twilio::TwiML::Response.new do |r|
-	  r.Sms "Sorry brah, locals only, please type 'spots' or a valid spot ID"
-	end	
+	response = Twilio::TwiML::Response.new  { |r| r.Sms "Sorry brah, locals only, please type 'spots' or a valid spot ID" }
 
 end
 
